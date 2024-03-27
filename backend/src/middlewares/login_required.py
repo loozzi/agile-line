fa7f9b9e8cb2098ba52ffd2760c307548065f043
unknown <vuthanhdat019@gmail.com>
2024-03-28 00:12:50 +0700

    fix error in login_required.py

--- backend/src/middlewares/login_required.py
+++ backend/src/middlewares/login_required.py
@@ -12,7 +12,7 @@ def token_required(f):
     def decorated(*args, **kwargs):
         token = None
         if "Authorization" in request.headers:
-            token = request.headers["Authorization"].split(" ")[1]
+            token = request.headers["Authorization"]
         if not token:
             return _response(
                 401, "Authentication Token is missing!", None, "Unauthorized"
