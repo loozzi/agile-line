0db07fef9a0b089eb7fa90553799c0b6c94f9d54
Nguyen Quang Thao <nquangthao19072004@gmail.com>
2024-04-16 14:45:02 +0700

    handle upload image to imgur

--- backend/src/controllers/__init__.py
+++ backend/src/controllers/__init__.py
@@ -1,3 +1,4 @@
 from src.controllers.auth_controller import auth
-from src.controllers.workspace_controller import workspace
+from src.controllers.file_controller import fileRoute
 from src.controllers.user_controller import user
+from src.controllers.workspace_controller import workspace
