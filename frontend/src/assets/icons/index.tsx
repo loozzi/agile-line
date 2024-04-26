5ffc76a83f8d5f334534b14a02c68b212cd20dba
Nguyen Quang Thao <nquangthao19072004@gmail.com>
2024-04-26 14:13:33 +0700

    add custom icon

--- frontend/src/assets/icons/index.tsx
+++ frontend/src/assets/icons/index.tsx
@@ -1,5 +1,23 @@
 import github from './github.svg'
 import usersGroup from './users-group.svg'
+import backlog from './backlog.svg'
+import planned from './planned.svg'
+import inprogress from './inprogress.svg'
+import completed from './completed.svg'
+import canceled from './canceled.svg'
+import paused from './paused.svg'
+import loader from './loader.svg'
+import calendarStats from './calendar-stats.svg'
+import calendarOff from './calendar-off.svg'
 
 export const GithubIcon = () => <img src={github} alt='github' />
 export const UsersGroupIcon = () => <img src={usersGroup} alt='users-group' />
+export const BacklogIcon = () => <img src={backlog} alt='backlog' />
+export const PlannedIcon = () => <img src={planned} alt='planned' />
+export const InprogressIcon = () => <img src={inprogress} alt='inprogress' />
+export const CompletedIcon = () => <img src={completed} alt='completed' />
+export const CanceledIcon = () => <img src={canceled} alt='canceled' />
+export const PausedIcon = () => <img src={paused} alt='paused' />
+export const LoaderIcon = () => <img src={loader} alt='loader' />
+export const CalendarStatsIcon = () => <img src={calendarStats} alt='calendar-stats' />
+export const CalendarOffIcon = () => <img src={calendarOff} alt='calendar-off' />
